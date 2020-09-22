46c45d54e932cd4d740a23b1793a3874b2398815
alexisnov <novosselski@gmail.com>
2020-09-22 12:55:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,27 +7,5 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	setlocale(0, "rus");
-	Book a; //First book
-	a.Print();
-	int n; //book amount
-	cout<<"Enter number of books \n";
-	cin>>n;
-	PrintBook *books = new PrintBook[n];
-	for (int i=0;i<n;i++){ //Ask for books
-		cout<<"Enter book #"<<i+1<<" Name: \n";
-		string name;
-		cin >> name;
-		cout<<"Enter book #"<<i+1<<" Pages: \n";
-		int p;
-		cin>>p;
-		PrintBook pbook;
-		pbook.setName(name); 
-		pbook.setPages(p);
-		books[i] = pbook;
-	}
-	for (int i=0;i<n;i++){ //Print books
-		books[i].Print();
-	}
 	return 0;
 }
